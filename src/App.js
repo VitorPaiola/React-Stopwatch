3dcad4608021d51397f4f6e2be5f0b617a53372a
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-07-20 00:31:34 -0300

    React-Cronômetro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./style.css"
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    constructor(props) {
+        super(props)
+        this.state = {
+            numero: 0,
+            botao: "INICIAR"
+        }
+        this.timer = null
+        this.vai = this.vai.bind(this)
+        this.limpar = this.limpar.bind(this)
+    }
 
-export default App;
+    vai() {
+        let state = this.state
+
+        if (this.timer !== null) {
+            clearInterval(this.timer)
+            this.timer = null
+            state.botao = "INICIAR"
+        } else {
+            this.timer = setInterval(() => {
+                let state = this.state
+                state.numero += 0.1
+                this.setState(state)
+            }, 100)
+            state.botao = "PAUSAR"
+        }
+        this.setState(state)
+    }
+
+    limpar() {
+        if (this.timer !== null) {
+            clearInterval(this.timer)
+            this.timer = null
+        }
+
+        let state = this.state
+        state.numero = 0
+        state.botao = "INICIAR"
+        // passando essas states p/ ele alterar tudo
+        this.setState(state) 
+    }
+    
+        render() {
+            return (
+                <div className="container">
+                    <img src={require("./assets/cronometro.png")} alt="" className="img" />
+                    <a href="/" className="timer">{this.state.numero.toFixed(1)}</a>
+                    <div className="areaBtn">
+                        <button className="botao" onClick={this.vai}>{this.state.botao}</button>
+                        <button className="botao" onClick={this.limpar}>LIMPAR</button>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+export default App
